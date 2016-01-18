8c5130e41a04ecaeb3b34aea1ad77e24b3d5cfbc
Francois Lebel <francoislebel@gmail.com>
2016-01-17 16:41:12 -0800

    Added support for running on FreeBSD, because subprocess.Popen doesn't behave like on a GNU/Linux platform.

--- rtsp2snapshot.py
+++ rtsp2snapshot.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
 
 import io
+import platform
 import shlex
 import subprocess
 
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, send_from_directory
 
 app = Flask(__name__)
 
+tmp_dir = '/tmp/'
+tmp_filename = 'snapshot.jpg'
+
 @app.route('/<path:url>')
 def snapshot(url):
+    freebsd_platform = platform.system() == 'FreeBSD'
     if request.query_string:
         url += '?' + request.query_string
     # TODO: Sanitize interpolated string
-    cmd = 'ffmpeg -i "rtsp://%s" -hide_banner -loglevel quiet -ss 00:00:01.500 -f image2 -vframes 1 -y -' % (url,)
+    cmd = 'ffmpeg -rtsp_transport tcp -i "rtsp://%s" -hide_banner -loglevel quiet -ss 00:00:01.500 -f image2 -vframes 1 -y ' % (url,)
+    if freebsd_platform:
+        cmd += tmp_dir + tmp_filename
+    else:
+        cmd += '-'
     p = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
     p.wait()
     image = p.stdout.read()
+    if freebsd_platform:
+        return send_from_directory(tmp_dir, tmp_filename)
     return send_file(io.BytesIO(image),
                      attachment_filename='snapshot.jpg',
                      mimetype='image/jpeg')
